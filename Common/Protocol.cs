38268eb4123477e20550f992f4fb691db2e2c813
Alexander Fomin <94113863+shashaf@users.noreply.github.com>
2026-05-26 17:35:07 +0300

    Написали команды сервера к клиенту

--- Common/Protocol.cs
+++ Common/Protocol.cs
@@ -7,4 +7,13 @@
         public const string PlacePixel = "PIXEL";
         public const string ListWorlds = "LIST";
     }
+
+    public static class ServerCommands
+    {
+        public const string WorldState = "STATE";
+        public const string PixelPlaced = "PIXEL_OK";
+        public const string Error = "ERROR";
+        public const string WorldList = "WORLDS";
+
+    }
 }
