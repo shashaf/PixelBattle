002544bd6e6f02d965e4bdf0df0428f3af10ab61
Alexander Fomin <94113863+shashaf@users.noreply.github.com>
2026-05-26 18:32:25 +0300

    Написал код класса для мира

--- ServerPB/World.cs
+++ ServerPB/World.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ServerPB
 {
-    internal class World
+    public class World
     {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public byte[,] Pixels { get; set; }
+        public List<string> Players { get; set; } = new List<string>();
+        public Dictionary<string, DateTime> LastPixelTime { get; set; } = new();
+        public Dictionary<string, int> PlayerScores { get; set; } = new(); 
+
+        public World(int id, string name, int width, int height) 
+        {
+            Id = id;
+            Name = name;
+            Width = width;
+            Height = height;
+            Pixels = new byte[width, height];
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    Pixels[x, y] = 0;
+        }
     }
 }
