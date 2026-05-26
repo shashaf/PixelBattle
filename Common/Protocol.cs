b6bd89040eb26000b3b14154da7ee0723f5bd2a3
Alexander Fomin <94113863+shashaf@users.noreply.github.com>
2026-05-26 17:30:24 +0300

    Написали команды клиента к серверу

---
+++ Common/Protocol.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public static class ClientCommands
+    {
+        public const string CreateWorld = "CREATE";
+        public const string JoinWorld = "JOIN";
+        public const string PlacePixel = "PIXEL";
+        public const string ListWorlds = "LIST";
+    }
+}
