10aa0abd179c2ee947f05ba1f374e60eb461a625
Alexander Fomin <sanfom24@gmail.com>
2026-05-30 19:29:43 +0300

    Добавили вывод IP-адресов для подключения к серверу

--- ServerPB/Program.cs
+++ ServerPB/Program.cs
@@ -1,4 +1,15 @@
 ﻿using ServerPB;
+using System.Net;
 
 var server = new GameServer();
+
+// Выводим все доступные IP
+Console.WriteLine("Сервер запущен. Доступные IP для подключения:");
+foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+{
+    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+        Console.WriteLine($"  {ip}");
+}
+
 server.Start();
+
